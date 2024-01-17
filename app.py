1b9303d381dbc568527fac3884ecf61482537c66
ken-diva <kendiva17@gmail.com>
2024-01-17 15:19:38 +0700

    add route

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello():
     return "Coba server!"
 
 
+@app.route("/route_baru")
+def route_baru():
+    return "Coba route baru!"
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=1234)
